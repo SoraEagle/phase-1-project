3009ed49ec3035cf93d9a6c336550671a77dbf4d
Matthew <phillips.matthew829@gmail.com>
2021-12-19 12:09:42 -0600

    add psuedocode for loading table data

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 //Write JS code here
-
 /*
 Psuedocode:
 
 function loadTable(){
     const table = getElementById("tableBody");
+    const searchResult = //The innerText of the text field;
+
     loop{
-        insert info
+        // insert info
+        fetch(`www.thecocktaildb.com/api/json/v1/1/search.php?s= //drink name`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+            body: JSON.stringify(//The table data),
+        })
     }
 }
 */

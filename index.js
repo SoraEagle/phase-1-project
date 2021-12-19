bee3c33dcb73500a6fbc8e1a52f85f8243d4f274
Matthew <phillips.matthew829@gmail.com>
2021-12-19 15:01:16 -0600

    fix index.js not working, add tableSubmit

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 //Write JS code here
 //Psuedocode:
 
-
 /*
 table: references the "tableBody" of the "cocktailsTable" in index.html.
 filter: References the innerText of the text field in index.html.
 */
-
 
 //Global scope variables:
 const table = document.getElementById("tableBody");
@@ -34,17 +32,14 @@ function loadTable(){
 }*/
 
 
-/*
 function tableSubmit(event){
     event.preventDefault(); //Prevent reloading of page.
-    //
-}*/
+    loadTable();
+}
 
-/*
+
 let drinkForm = document.getElementById("drink-form");
 drinkForm.addEventListener("submit", tableSubmit);
-*/
-
 
 //Use addEventListener("change", ()=>{}) to change filter with text field's innerText
 document.addEventListener("change", () => {

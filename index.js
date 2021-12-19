f4357d726f4bc54044c690a12691f6787b118a87
Matthew <phillips.matthew829@gmail.com>
2021-12-19 13:28:12 -0600

    modify pseudocode in index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 //Psuedocode:
 
 //Global scope variables:
+const table = document.getElementById("tableBody");
+const filter = document.getElementByID("drink").innerText; //The innerText of the text field;
 
 
-
-// Use DOMContentLoaded:
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOM has loaded");
     const searchBtn = document.querySelector("#search");
@@ -16,12 +16,31 @@ document.addEventListener("DOMContentLoaded", () => {
 function loadTable(){
     let tableUrl = "www.thecocktaildb.com/api/json/v1/1/search.php?s="; //The url, except for the search word.
 
-    "submit"{
-        // insert info
-        fetch(`tableUrl${filter}`, {
-            method: 'GET',
-        })
-    }
+    //Fetch table data
+    fetch(`tableUrl${filter}`, {
+        method: 'GET',
+    })
+    .then(response => response.json()) //Converts response to JSON
+    .then(data => {
+        //
+    })
 }*/
 
 
+/*
+function tableSubmit(event){
+    event.preventDefault(); //Prevent reloading of page.
+    //
+}*/
+
+/*
+let drinkForm = document.getElementsByTagName("form")[0];
+drinkForm.addEventListener("submit", tableSubmit);
+*/
+
+
+//Use addEventListener("change", ()=>{}) to change search filter with text field's innerText
+/*
+document.addEventListener("change", () => {
+    //
+})*/

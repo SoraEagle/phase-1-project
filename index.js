e6c578a387ddc881270b7d24288e9e82aa0aed61
Matthew <phillips.matthew829@gmail.com>
2021-12-20 11:19:41 -0600

    add functions createRows and deleteRows

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ filter: References the innerText of the text field in index.html.
 
 //Global scope variables:
 const table = document.getElementById("tableBody");
-const filter = document.getElementById("drink").innerText; //The innerText of the text field;
+let filter = document.getElementById("drink").innerText; //The innerText of the text field;
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -17,19 +17,30 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 
-/*
 function loadTable(){
-    let tableUrl = "www.thecocktaildb.com/api/json/v1/1/search.php?s="; //The url, except for the search word.
+    let tableUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"; //The url, except for search word.
+
 
     //Fetch table data
-    fetch(`tableUrl${filter}`, {
+    fetch(tableUrl, {
         method: 'GET',
     })
     .then(response => response.json()) //Converts response to JSON
     .then(data => {
-        //
+        // debugger;
+        console.log(data);
     })
-}*/
+}
+
+
+function deleteRows(){ //Function to delete rows before new table is generated
+    //
+}
+
+
+function createRows(){ //Function to add rows of newly generated table
+    //
+}
 
 
 function tableSubmit(event){
@@ -41,7 +52,10 @@ function tableSubmit(event){
 let drinkForm = document.getElementById("drink-form");
 drinkForm.addEventListener("submit", tableSubmit);
 
-//Use addEventListener("change", ()=>{}) to change filter with text field's innerText
-document.addEventListener("change", () => {
-    filter = getElementById("drink").innerText;
-})
+//Functon to use addEventListener("change", ()=>{}) to change filter with text field's innerText
+function tableFilter(){
+    document.addEventListener("change", () => {
+        filter = getElementById("drink").innerText;
+        return filter;
+    })
+}
